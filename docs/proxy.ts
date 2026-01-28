9c8bbb66fce6c4d431ce3c15b10028cb9f1748f4
voyager14 <21mh124@queensu.ca>
2026-01-28 00:06:59 -0500

    exclude og in middleware
    Signed-off-by: voyager14 <21mh124@queensu.ca>

--- docs/proxy.ts
+++ docs/proxy.ts
@@ -40,8 +40,8 @@ const proxy = (request: NextRequest, context: NextFetchEvent) => {
 };
 
 export const config = {
-  // Matcher ignoring `/_next/`, `/api/`, static assets, favicon, etc.
-  matcher: ['/((?!sitemap.xml|api|_next/static|_next/image|favicon.ico).*)'],
+  // Matcher ignoring `/_next/`, `/api/`, `/og/`, static assets, favicon, etc.
+  matcher: ['/((?!sitemap.xml|api|og|_next/static|_next/image|favicon.ico).*)'],
 };
 
 export default proxy;
